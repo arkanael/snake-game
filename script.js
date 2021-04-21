80f11d1a0f6533430614bd7f283017a94e9c4e79
Luiz Guilherme Bandeira <arkanael@gmail.com>
2021-04-21 08:24:49 -0300

    Feito a lógica para aumentar o tamanho da cobrinha

--- script.js
+++ script.js
@@ -52,6 +52,8 @@ function update(event){
         direction = 'down';
     }
 }
+let snakeX = snake[0].x;
+let snakeY =snake[0].y;
 
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == 'rigth') snake[0].x = 0;
@@ -59,19 +61,31 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+    for(i =1 ; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over!!!")
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
-    
+
     let snakeX = snake[0].x;
-    let snakeY =snake[0].y;
+    let snakeY = snake[0].y;
 
     if (direction == 'rigth')  snakeX += box;
     if (direction == 'left')  snakeX -= box;
     if (direction == 'up')  snakeY -= box;
     if (direction == 'down')  snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.X || snakeY != food.Y){
+        snake.pop();
+    }else{
+        food.X = Math.floor(Math.random() * 15 + 1) * box;
+        food.Y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX, 
